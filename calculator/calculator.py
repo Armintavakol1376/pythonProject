c5ff368d6e367c161d879a694237e7b393d76619
Armintavakol1376 <130907754+Armintavakol1376@users.noreply.github.com>
2023-04-21 12:07:54 +0330

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,39 +1,33 @@
 # Simple Calculator
 
-def add(x, y):
-    return x + y
-
-
-def subtract(x, y):
-    return x - y
-
-
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
-    return x / y
-
-
-operations = {'1': add, '2': subtract, '3': multiply, '4': divide}
-
 print("Select operation.")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 
+choice = input("Enter choice (1/2/3/4): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
-while True:
-    choice = input("Enter choice (1/2/3/4): ")
+if choice == '1':
+    result = num1 + num2
+    print("Result:", result)
 
-    if choice in operations:
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+elif choice == '2':
+    result = num1 - num2
+    print("Result:", result)
 
-        result = operations[choice](num1, num2)
+elif choice == '3':
+    result = num1 * num2
+    print("Result:", result)
+
+elif choice == '4':
+    if num2 == 0:
+        print("Error: division by zero")
+    else:
+        result = num1 / num2
         print("Result:", result)
 
-    else:
-        print("Invalid input. Please try again.")
+else:
+    print("Invalid input. Please try again.")
