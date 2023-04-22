fda42fa198e91d7d12f0b483f1307cf2312c9dca
Armin_Tavakol <armintavakol111177@gmail.com>
2023-04-22 13:56:23 +0430

    I corrected it

--- gussing_game.py
+++ gussing_game.py
@@ -1,1 +1,23 @@
-git
+# first I need produce random number.
+import random
+
+num = random.randint(1, 100)
+guess = None
+tries = 0
+
+print(" whats your guess number?")
+
+while guess != num:
+    guess = int(input("inter your guess :"))
+    tries += 1
+
+    if num > guess:
+        print("its too short")
+
+    elif num < guess:
+        print("its too high")
+
+    else:
+        print(("Congratulations, you guessed it in", tries, "tries!"))
+
+
